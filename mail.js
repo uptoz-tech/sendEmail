49a4f5035bb846d1d7573daaf025bb2b2ecf53fe
Sooriya <info.uptoz@gmail.com>
2023-01-30 22:25:32 +0530

    request data added

--- mail.js
+++ mail.js
@@ -1,11 +1,20 @@
 var elasticemail = require('elasticemail');
 const express = require('express')
+
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 
-app.post('/create', function (req, res) {
+app.use(bodyParser.json())
 
+app.post('/create', function (req, res) {
+    console.log(req.body,'data')
+
+    const to = req.body.toemail
+    const fname = req.body.fname
+    const lname = req.body.lname
+    const num = req.body.pnum
     var client = elasticemail.createClient({
         username: 'info.uptoz@gmail.com',
         apiKey: '2B682661630CC395424CE7A8960F6870A10223BB376CD1DDBD0B033482E49DED62BF8ABF867B29CDE5141CF5BA972875'
@@ -13,11 +22,10 @@ app.post('/create', function (req, res) {
 
     var msg = {
         from: 'info.uptoz@gmail.com',
-        from_name: 'Sooriya',
-        to: 'sooriyanadarajan@gmail.com',
+        from_name: 'UptoZ Technologies',
+        to: to,
         subject: 'Intimation Mail for View State',
-        body_text: 'helo '
-        // 'Hello Boss!,         First name and Last name tried to reach You !'
+        body_text: 'Hello Boss!, '        + fname + lname +  ' tried to reach You ! , Please Contact Back (' + num  + ')' 
     };
 
     client.mailer.send(msg, function (err, result) {
